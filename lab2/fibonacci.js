cdd58996c56e82e6fc5f8532375111bae1afe1b3
EliltaW <eliltawondimu12@gmail.com>
2022-05-23 22:56:14 -0500

    lab 1 done

--- lab2/fibonacci.js
+++
@@ -1,6 +0,0 @@
-const child_process = require("child_process");
-console.log("1: Start");
-const newProcess = child_process.spawn("node", ["fibonacci30and15.js"], {
-  stdio: "inherit",
-});
-console.log("2: End");
